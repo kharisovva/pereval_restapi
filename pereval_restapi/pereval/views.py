31153d6772b7ebce995b66ee0217bfde29a11433
Karina <harisova_k@mail.ru>
2025-04-14 21:36:52 +0500

    Hot fixes

--- pereval_restapi/pereval/views.py
+++ pereval_restapi/pereval/views.py
@@ -1,5 +1,8 @@
+import json
+
 from django.db import DatabaseError
 from rest_framework import status as http_status
+from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -8,17 +11,39 @@ from pereval.serializers import SubmitDataSerializer
 
 
 class SubmitDataView(APIView):
-    def post(self, request):
-        serializer = SubmitDataSerializer(data=request.data)
-        if not serializer.is_valid():
-            return Response(
-                {"status": 400, "message": serializer.errors, "id": None}, status=http_status.HTTP_400_BAD_REQUEST
-            )
+    parser_classes = (MultiPartParser, FormParser)
 
+    def post(self, request):
         try:
+            data = json.loads(request.data.get("data", "{}"))
+            serializer = SubmitDataSerializer(data=data)
+            if not serializer.is_valid():
+                return Response(
+                    {"status": 400, "message": serializer.errors, "id": None}, status=http_status.HTTP_400_BAD_REQUEST
+                )
+
+            image_files = request.FILES.getlist("images", [])
+            images_data = serializer.validated_data.get("pereval", {}).get("images", [])
+
+            if images_data and len(image_files) != len(images_data):
+                return Response(
+                    {
+                        "status": 400,
+                        "message": "Количество загруженных файлов не совпадает с количеством заголовков",
+                        "id": None,
+                    },
+                    status=http_status.HTTP_400_BAD_REQUEST,
+                )
+
             manager = PerevalDataManager()
-            pereval = manager.submit_data(serializer.validated_data)
+            pereval = manager.submit_data(serializer.validated_data, image_files)
             return Response({"status": 200, "message": None, "id": pereval.id}, status=http_status.HTTP_200_OK)
+
+        except json.JSONDecodeError:
+            return Response(
+                {"status": 400, "message": "Некорректный формат JSON в поле data", "id": None},
+                status=http_status.HTTP_400_BAD_REQUEST,
+            )
         except DatabaseError:
             return Response(
                 {"status": 500, "message": "Ошибка подключения к базе данных", "id": None},
