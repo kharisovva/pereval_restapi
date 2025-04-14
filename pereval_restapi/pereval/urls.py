765f1a5cd35e1016d84730f43ad012615eb3d5f3
Karina <harisova_k@mail.ru>
2025-04-14 19:16:57 +0500

    Added POST method

--- pereval_restapi/pereval/urls.py
+++ pereval_restapi/pereval/urls.py
@@ -1,1 +1,6 @@
-urlpatterns = []
+from django.urls import path
+from pereval.views import SubmitDataView
+
+urlpatterns = [
+    path('submitData/', SubmitDataView.as_view(), name='submit_data'),
+]
